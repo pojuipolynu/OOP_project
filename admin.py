d58eec00e8092fcea5309c257d6b8f54aae67631
Marina Barabash <113929861+Aylosteraa@users.noreply.github.com>
2022-11-30 18:13:50 +0200

    Update admin.py

--- admin.py
+++ admin.py
@@ -6,9 +6,14 @@ TOKEN = 'token'
 
 bot = telebot.TeleBot(TOKEN)
 
-admin_id = [1234]
+admin_id = [12345]
 btn_list = ['Title', 'Author', 'Artist', 'Genre', 'Periodicity', 'Magazine', 'Chapters', 'Status', 'Colorization', 'Kind', 'Adaptation', 'Translation', 'Stop']
 answer_dict = {}
+periodicity_list = ['every day', 'every week', 'every month', 'non-periodical']
+kind_list = ['manga', 'manhwa', 'manhua', 'comics', 'maliopys']
+status_list = ['frozen', 'ongoing', 'completed']
+colorization_list = ['monochrome', 'non-monochrome', 'lineart']
+adaptation_list = ['film', 'cartoon', 'tvshow', 'show', 'anime']
 
 
 @bot.message_handler(chat_id=admin_id, commands=['start']) 
@@ -33,6 +38,11 @@ def create_keyboards(button_list, width=2):
 	markup = types.ReplyKeyboardMarkup(row_width=width)
 	markup = generate_buttons(button_list, markup)
 	return markup
+
+def return_to_main(message, key):
+	store_answer(message, key)
+	markup = create_keyboards(btn_list)
+	bot.reply_to(message, 'Choose what you want to add', reply_markup=markup)
 	
 @bot.message_handler(content_types=['text'])
 def store_answer(message, key):
@@ -45,114 +55,51 @@ def generate_buttons(button_list, markup):
         markup.add(types.KeyboardButton(button))
     return markup
 
-
-@bot.message_handler(chat_id=admin_id)
-def title_quation(message):
-	bot.reply_to(message, "Enter title of comics")
-	bot.register_next_step_handler(message, store_answer, 'name')
-
-@bot.message_handler(chat_id=admin_id)
-def author_quation(message):
-	bot.reply_to(message, "Enter author of comics")
-	bot.register_next_step_handler(message, store_answer, 'author')
-
-@bot.message_handler(chat_id=admin_id)
-def artist_quation(message):
-	bot.reply_to(message, "Enter artist of comics")
-	bot.register_next_step_handler(message, store_answer, 'artist')
-
-@bot.message_handler(chat_id=admin_id)
-def genre_quation(message):
-	bot.reply_to(message, "Enter genre of comics")
-	bot.register_next_step_handler(message, store_answer, 'genre')
-
-@bot.message_handler(chat_id=admin_id)
-def magazine_quation(message):
-	bot.reply_to(message, "Enter magazine")
-	bot.register_next_step_handler(message, store_answer, 'magazine')
-
-@bot.message_handler(chat_id=admin_id)
-def periodicity_quation(message):
-	periodicity_list = ['every day', 'every week', 'every month', 'non-periodical']
-	markup = create_keyboards(periodicity_list)
-	message = bot.reply_to(message, 'Choose periodicity of comics', reply_markup=markup)
-	bot.register_next_step_handler(message, return_to_main, 'periodicity')
-	
-
-@bot.message_handler(chat_id=admin_id)
-def kind_quation(message):
-	kind_list = ['manga', 'manhwa', 'manhua', 'comics', 'maliopys']
-	markup=create_keyboards(kind_list)
-	message = bot.reply_to(message, 'Choose kind of comics', reply_markup=markup)
-	bot.register_next_step_handler(message, return_to_main, 'kind')
-
-def return_to_main(message, key):
-	store_answer(message, key)
-	markup = create_keyboards(btn_list)
-	bot.reply_to(message, "Insert comics.", reply_markup=markup)
-
 @bot.message_handler(chat_id=admin_id)
 def incorrect_message(message):
 	bot.reply_to(message, "I don't recognize your messege")
 	bot.register_next_step_handler(message, bot_asks)
 
 @bot.message_handler(chat_id=admin_id)
-def chapters_quation(message):
-	bot.reply_to(message, "Enter number of comics' chapters")
-	bot.register_next_step_handler(message, store_answer, 'chapter')
-
-@bot.message_handler(chat_id=admin_id)
-def status_quation(message):
-	status_list = ['frozen', 'ongoing', 'completed']
-	markup = create_keyboards(status_list)
-	message = bot.reply_to(message, 'Choose status of comics', reply_markup=markup)
-	bot.register_next_step_handler(message, return_to_main, 'status')
-
-@bot.message_handler(chat_id=admin_id)
-def colorization_quation(message):
-	colorization_list = ['monochrome', 'non-monochrome', 'lineart']
-	markup = create_keyboards(colorization_list)
-	message = bot.reply_to(message, 'Choose colorization', reply_markup=markup)
-	bot.register_next_step_handler(message, return_to_main, 'colorization')
-
-@bot.message_handler(chat_id=admin_id)
-def adaptation_quation(message):
-	adaptation_list = ['film', 'cartoon', 'tvshow', 'show', 'anime']
-	markup = create_keyboards(adaptation_list)
-	message = bot.reply_to(message, 'Choose colorization', reply_markup=markup)
-	bot.register_next_step_handler(message, return_to_main, 'adaptation')
-
-
-@bot.message_handler(chat_id=admin_id)
 def stop(message):
 	bot.reply_to(message, "The end of inserting")
 
+@bot.message_handler(chat_id=admin_id)
+def quation(message, quation, key):
+	bot.reply_to(message, quation)
+	bot.register_next_step_handler(message, store_answer, key)
+
+@bot.message_handler(chat_id=admin_id)
+def quation_choose(message, button_list, quation, key):
+	markup = create_keyboards(button_list)
+	message = bot.reply_to(message, quation, reply_markup=markup)
+	bot.register_next_step_handler(message, return_to_main, key)
 
 def bot_asks(message):
 	if message.text == 'Title':
-		title_quation(message)
+		quation(message, "Enter title of comics", 'name')
 	elif message.text == 'Author':
-		author_quation(message)
+		quation(message, "Enter author of comics", 'author')
 	elif message.text == 'Artist':
-		artist_quation(message)
+		quation(message, "Enter artist of comics", 'artist')
 	elif message.text == 'Genre':
-		genre_quation(message)
+		quation(message, "Enter genre of comics", 'genre')
 	elif message.text == 'Periodicity':
-		periodicity_quation(message)
+		quation_choose(message, periodicity_list, 'Choose periodicity of comics', 'periodicity')
 	elif message.text == 'Magazine':
-		magazine_quation(message)
+		quation(message, "Enter magazine", 'magazine')
 	elif message.text == 'Chapters':
-		chapters_quation(message)
+		quation(message, "Enter number of comics' chapters", 'chapter')
 	elif message.text == 'Status':
-		status_quation(message)
+		quation_choose(message, status_list, 'Choose status of comics', 'status')
 	elif message.text == 'Colorization':
-		colorization_quation(message)
+		quation_choose(message, colorization_list, 'Choose colorization', 'colorization')
 	elif message.text == 'Kind':
-		kind_quation(message)
+		quation_choose(message, kind_list, 'Choose kind of comics', 'kind')
 	elif message.text == 'Adaptation':
-		adaptation_quation(message)
+		quation_choose(message, adaptation_list, 'Choose colorization', 'adaptation')
 	elif message.text == 'Translation':
-		magazine_quation(message)
+		quation(message, "Enter translation", 'translation')
 	elif message.text == 'Stop':
 		stop(message)
 	else:

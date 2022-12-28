880822280dcbc820ddd09fa79aae30afcc5cf7a3
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-12-28 20:05:35 +0200

    Update _comics_database.py

--- _comics_database.py
+++ _comics_database.py
@@ -262,3 +262,71 @@ class DBUpdating:
         mycursor.execute(sql, vall)
         self.mydb.commit()       
  
+class DBDeleting:
+
+    def delete_comic(self, name):
+        mycursor = self.mydb.cursor()
+        sql = "DELETE FROM comics WHERE idcomics = %s;"
+        val = self.search_comics(name)
+        vall = list()
+        vall += ([x[0] for x in val])
+        mycursor.execute(sql, vall)
+        self.mydb.commit()
+
+
+class DBPrinting:
+
+    def print_comics(self, name):
+        mycursor = self.mydb.cursor()
+        sql = "SELECT\
+              comics.name AS ncomic,\
+              comics.num_chapter AS chapter,\
+              author.name AS nauthor,\
+              author.surname AS sauthor,\
+              artist.name AS nartist,\
+              artist.surname AS sartist,\
+              kind.name AS kname,\
+              genre.subgenre AS subgenre,\
+              genre.name AS gname,\
+              periodicity.period AS pperiod,\
+              magazine.title AS mtitle,\
+              status.name AS sname,\
+              colourization.name AS cname,\
+              adaptation.type AS atype,\
+              translation.language AS tlanguage,\
+              translation.legality AS tlegality\
+              FROM comics\
+              INNER JOIN genre ON comics.genre_idgenre = genre.idgenre\
+              INNER JOIN author ON comics.author_idauthor = author.idauthor\
+              INNER JOIN artist ON comics.artist_idartist = artist.idartist\
+              INNER JOIN periodicity ON comics.periodicity_idperiodicity = periodicity.idperiodicity\
+              INNER JOIN magazine ON comics.magazine_idmagazine = magazine.idmagazine\
+              INNER JOIN status ON comics.status_idstatus = status.idstatus\
+              INNER JOIN colourization ON comics.colourization_idcolourization = colourization.idcolourization\
+              INNER JOIN kind ON comics.kind_idkind = kind.idkind\
+              INNER JOIN adaptation ON comics.adaptation_idadaptation = adaptation.idadaptation\
+              INNER JOIN translation ON comics.translation_idtranslation = translation.idtranslation\
+              WHERE idcomics = %s;"
+        vall = list()
+        if isinstance(name[0], str):
+            val = self.search_comics(name)
+            vall += ([x[0] for x in val])
+        else:
+            vall += name
+        mycursor.execute(sql, vall)
+        myresult = mycursor.fetchall()
+        return myresult
+
+    def print_user(self, id):
+        mycursor = self.mydb.cursor()
+        sql = "SELECT liked FROM user WHERE iduser = %s;"
+        vall = list()
+        vall.append(id)
+        mycursor.execute(sql, vall)
+        myresult = mycursor.fetchall()
+        return myresult
+    
+
+
+    
+ 
